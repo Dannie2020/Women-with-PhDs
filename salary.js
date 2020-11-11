19109794b6178b12e155c59d44b4d8923c7b7150
dannie.gao <dannie.gao@cgi.com>
2020-11-10 22:33:32 -0800

    added plot by function

--- salary.js
+++ salary.js
@@ -1,20 +1,19 @@
 // build the legend
 let salaryContainer = d3.select("#salaryContainer")
-salaryContainer.append("circle").attr("cx", 340).attr("cy", 60).attr("r", 6).style("fill", "#e8505b")
-salaryContainer.append("circle").attr("cx", 440).attr("cy", 60).attr("r", 6).style("fill", "#16a596")
-salaryContainer.append("text").attr("x", 350).attr("y", 60).text("Female").style("font-size", "15px").attr("alignment-baseline", "middle")
-salaryContainer.append("text").attr("x", 450).attr("y", 60).text("Male").style("font-size", "15px").attr("alignment-baseline", "middle")
+salaryContainer.append("circle").attr("cx", 390).attr("cy", 60).attr("r", 6).style("fill", "#e8505b")
+salaryContainer.append("circle").attr("cx", 490).attr("cy", 60).attr("r", 6).style("fill", "#16a596")
+salaryContainer.append("text").attr("x", 400).attr("y", 60).text("Female").style("font-size", "15px").attr("alignment-baseline", "middle")
+salaryContainer.append("text").attr("x", 500).attr("y", 60).text("Male").style("font-size", "15px").attr("alignment-baseline", "middle")
 
 // bind the raw data and show the visualization
 let data = d3.csv("salary_visual_data.csv", function (d) {
     return {
         job: d.N2OCPRNG,
-        fSalary: +d.femaleSalary,
-        mSalary: +d.maleSalary,
-        gap: +d.maleSalary - +d.femaleSalary
+        fSalary: +d.FemaleSalary,
+        mSalary: +d.MaleSalary,
+        gap: +d.Gap
     }
 }).then(showData)
-
 
 // create plot container and its variables
 let plotContainer = d3.select("#salaryPlotContainer")
@@ -52,18 +51,24 @@ function showData(data) {
     plotContainer.append("text")
         .attr("transform", `translate(${plotVars.plotWidth / 2}, ${-40})`)
         .attr("class", "label")
+        .attr("id", "xlabel")
         .style("text-anchor", "middle")
         .text("Average Salary ($)")
 
-    // gridlines in x axis function
+    // gridlines for x and y axis function
     function make_x_gridlines() {
         return d3.axisBottom(xScale)
             .ticks()
     }
 
+    function make_y_gridlines() {
+        return d3.axisLeft(yScale)
+            .ticks()
+    }
+
     // add the X gridlines
     plotContainer.append("g")
-        .attr("class", "grid")
+        .attr("class", "xgrid")
         .attr("transform", "translate(0," + plotVars.plotHeight + ")")
         .call(make_x_gridlines()
             .tickSize(-plotVars.plotHeight)
@@ -86,15 +91,34 @@ function showData(data) {
         .attr("class", "y-axis")
         .call(yAxis)
 
+    yAxisGroup.selectAll('.tick')
+        .each(function (d) {
+            if (d == "Biological, agricultural and other life scientists " || d == "Computer and mathematical scientists " || d == "Engineers " || d == "Non-S&E Occupations " || d == "Physical and related scientists " || d == "S&E related occupations " || d == "Social and related scientists ") {
+                d3.select(this)
+                    .selectAll('text')
+                    .style("font-size", "13px")
+                    .style("font-weight", "bold")
+
+                plotContainer.append("g")
+                    .attr("class", "ygrid")
+                    .call(make_y_gridlines()
+                        .tickSize(-plotVars.plotWidth)
+                        .tickFormat("")
+                        .tickValues(["Biological, agricultural and other life scientists ", "Computer and mathematical scientists ", "Engineers ", "Non-S&E Occupations ", "Physical and related scientists ", "S&E related occupations ", "Social and related scientists "])
+                    )
+
+            }
+        })
+
     // create y axis label
     plotContainer.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - 300)
+        .attr("y", 0 - 350)
         .attr("x", 0 - (plotVars.plotHeight / 2))
         .attr("class", "label")
         .attr("dy", "1em")
         .style("text-anchor", "middle")
-        .text("Category of job")
+        .text("Principal Job Category/Sub-category")
 
     // lines
     plotContainer.selectAll("myline")
@@ -107,6 +131,7 @@ function showData(data) {
         .attr("y2", function (d) { return yScale(d.job); })
         .attr("stroke", "grey")
         .attr("stroke-width", "1px")
+        .attr("class", "plotLine")
 
     // circles of female salaries
     plotContainer.selectAll("mycircle")
@@ -176,7 +201,94 @@ function showData(data) {
             .style("opacity", 0);
     }
 
-    /// add sort
+    // add plot
+    d3.select("#plotBy")
+        .on("change", function () {
+            var attribute = d3.select(this).property("value");
+            plotLollipops(attribute);
+        })
+
+    function plotLollipops(attribute) {
+        if (attribute == "Gap") {
+            xScale.domain([0, 100])
+            xAxis.scale(xScale)
+                .tickFormat(function (d, i) {
+                    if (i == 0) {
+                        return "0%"
+                    } else {
+                        return d3.format(".2s")(d);
+                    }
+                })
+
+            d3.select(".x-axis").call(xAxis)
+
+            d3.selectAll(".plotLine")
+                .transition()
+                .duration(500)
+                .attr("x1", 0)
+                .attr("x2", function (d) { return xScale(d.gap); })
+                .attr("y1", function (d) { return yScale(d.job); })
+                .attr("y2", function (d) { return yScale(d.job); })
+
+            d3.selectAll(".lollipop-female")
+                .transition()
+                .duration(500)
+                .attr("cx", function (d) { return xScale(d.gap); })
+                .attr("cy", function (d) { return yScale(d.job); })
+                .attr("r", "6")
+
+            d3.selectAll(".lollipop-male")
+                .transition()
+                .duration(500)
+                .attr("cx", 0)
+                .attr("cy", function (d) { return yScale(d.job); })
+                .attr("r", "6")
+
+            d3.select("#xlabel")
+                .text("Male and Femal Average Salary Gap (%)")
+        }
+        else {
+            xScale.domain([0, 200000])
+            xAxis.scale(xScale)
+                .tickFormat(function (d, i) {
+                    if (i == 0) {
+                        return "$0"
+                    } else {
+                        return d3.format(".2s")(d);
+                    }
+                })
+
+            d3.select(".x-axis").call(xAxis)
+
+            d3.selectAll(".plotLine")
+                .transition()
+                .duration(500)
+                .attr("x1", function (d) { return xScale(d.fSalary); })
+                .attr("x2", function (d) { return xScale(d.mSalary); })
+                .attr("y1", function (d) { return yScale(d.job); })
+                .attr("y2", function (d) { return yScale(d.job); })
+
+            d3.selectAll(".lollipop-female")
+                .transition()
+                .duration(500)
+                .attr("cx", function (d) { return xScale(d.fSalary); })
+                .attr("cy", function (d) { return yScale(d.job); })
+                .attr("r", "6")
+
+            d3.selectAll(".lollipop-male")
+                .transition()
+                .duration(500)
+                .attr("cx", function (d) { return xScale(d.mSalary); })
+                .attr("cy", function (d) { return yScale(d.job); })
+                .attr("r", "6")
+
+            d3.select("#xlabel")
+                .text("Average Salary ($)")
+        }
+    }
+
+    // add sort
+    /*
     d3.select("#sortBy")
         .on("change", function () {
             var attribute = d3.select(this).property("value");
@@ -210,7 +322,7 @@ function showData(data) {
     sortBy(data, "Job Category", 1);
     yScale.domain(data.map(function (d) { return d.job }));
     xScale.domain([0, d3.max(data, function (d) { return d.max })]);
-    xScale.nice();
+    xScale.nice();*/
 
 }
 
